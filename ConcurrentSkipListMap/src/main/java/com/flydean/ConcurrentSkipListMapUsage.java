87420ed770533f29798aa6b40a49d4e6c6ad2949
wayne <wayne.fu@jfz.com>
2020-04-22 11:07:52 +0800

    concurrentskiplistmap

---
+++ ConcurrentSkipListMap/src/main/java/com/flydean/ConcurrentSkipListMapUsage.java
@@ -0,0 +1,18 @@
+package com.flydean;
+
+import org.junit.Test;
+
+import java.util.concurrent.ConcurrentSkipListMap;
+
+/**
+ * @author wayne
+ * @version ConcurrentSkipListMapUsage,  2020/4/21 4:52 下午
+ */
+
+public class ConcurrentSkipListMapUsage {
+
+    @Test
+    public void usage(){
+        ConcurrentSkipListMap concurrentSkipListMap=new ConcurrentSkipListMap();
+    }
+}
